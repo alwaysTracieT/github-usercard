e17dee7f951e4972984f7fed1adf4ad76afae585
Tracie T <traciecodes@gmail.com>
2019-07-12 08:22:12 -0500

    added my own card to page.

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,25 +1,4 @@
-//Steps 4 & 5
-const followersArray = [];
-axios.get('https://api.github.com/users/alwaysTracieT/followers')
-.then(data => {
-  console.log('Returned the following results: ', data);
-  data.data.forEach(element => {followersArray.push(element.login)});
-  console.log('Current followers: ', followersArray)
-})
-.catch (error => {
-  console.log('The following error has occured with followersArray: ', error)
-})
 
-followersArray.forEach(follower => {
-  axios.get(`https://api.github.com/users/${follower}`)
-  .then(data => {
-    createCard(data);
-    console.log('Card created for: ', follower);
-  })
-  .catch(error => {
-    console.log('Check your follower-card-creator: ', error);
-  })
-})
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -48,53 +27,84 @@ followersArray.forEach(follower => {
   luishrd
   bigknell
 */
-
+constcards = document.querySelector('.cards');
 //Step 1 & 2:
-// const promise = axios.get('https://api.github.com/users/alwaysTracieT')
-
+axios.get('https://api.github.com/users/alwaysTracieT')
 // promise
-//   .then(data => {
-//     //results from server
-//     console.log('response', data)
-//   })
-//   .catch(error => {
-//     console.log("Site no worky, sorry boutcha", error)
-//   })
+  .then(data => {
+    //results from server
+    console.log('response', data.data);
+    const card = createCard(data.data);
+    cards.append(card);
+  })
+  .catch(error => {
+    console.log("Request no worky, sorry boutcha", error)
+  })
 
+//Steps 4 & 5
+const followersArray = [];
+axios.get('https://api.github.com/users/alwaysTracieT/followers')
+.then(data => {
+  console.log('Returned the following results: ', data);
+  data.data.forEach(element => {followersArray.push(element.login)});
+  console.log('Current followers: ', followersArray)
+})
+.catch (error => {
+  console.log('The following error has occured with followersArray: ', error)
+})
+
+followersArray.forEach(follower => {
+  axios.get(`https://api.github.com/users/${follower}`)
+  .then(data => {
+    createCard(data);
+    console.log('Card created for: ', follower);
+  })
+  .catch(error => {
+    console.log('Check your follower-card-creator: ', error);
+  })
+})
 
 
 //Step 3: 
+const cards = document.querySelector('.cards');
 
-const thatKide = createCard();
-
-function createCard(data) {
+function createCard(user) {
   //create the elements
-  const card = document.createElement('card')
-  const userImg = document.createElement('img')
-  const cardInfo = document.createElement('card-info')
-  const fullName = document.createElement('name')
-  const userName = document.createElement('username')
+  const card = document.createElement('div'),
+    userImg = document.createElement('img'),
+    cardInfo = document.createElement('div'),
+    fullName = document.createElement('h3'),
+    userName = document.createElement('p'),
+    location = document.createElement('p'),
+    profile = document.createElement('p'),
+    link = document.createElement('a'),
+    followers = document.createElement('p'),
+    following = document.createElement('p'),
+    bio = document.createElement('p')
 
   //set the styles
-  card.classList.add('card')
-  userImg.classList.add('img')
-  cardInfo.classList.add('card-info')
-  fullName.classList.add('name')
-  userName.classList.add('username')
-
+  card.classList.add('card');
+  userImg.classList.add('img');
+  cardInfo.classList.add('card-info');
+  fullName.classList.add('fullName');
+  userName.classList.add('username');
+  
   //put in content
-  fullName.textContent = data.data.name;
-  userName.textContent = data.data.login;
-  userLocation.textContent = `Location ${data.data.location}`;
-  userProfile.textContent = `Profile ${data[profileLink]}`;
-  userFollowers.textContent = `Followers ${data[followers_url]}`;
-  userFollowing.textContent = `Following ${data[following_url]}`;
-  userBio.textContent = `User Bio: ${data[bio]}`;
-
-  profileLink.href = data[html_url];
-  userImage.src = data[avatar_url];
-  userImage.alt = "Image of the User"
+  userImg.src = user.avatar_url;
+  fullName.textContent = user.name;
+  userName.textContent = user.login;
+  location. textContent = `Location: ${user.location}`;
+  profile.textContent = 'Profile: ';
+  link.href = user.html_url;
+  link.textContent = user.html_url;
+  followers.textContent = `Followers: ${user.followers}`;
+  following.textContent = `Following: ${user.following}`;
+  bio.textContent = `Bio: ${user.bio}`;
+  //appends
+  card.append(userImg);
+  card.append(cardInfo);
+  cardInfo.append(userName, location, profile, followers, following, bio);
+  profile.append(link);
 
   return card;
-
 }
